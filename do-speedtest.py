21bd42ed36666edcdf6d695027cdcf7f7f66a7ca
Dominik Fringeli <dfringeli@outlook.com>
2020-03-28 16:12:18 +0100

    used speedtest

---
+++ do-speedtest.py
@@ -0,0 +1,23 @@
+import speedtest
+from scapy.all import *
+
+# interfaces
+interfaces = get_windows_if_list()
+print(interfaces)
+
+servers = []
+# If you want to test against a specific server
+# servers = [1234]
+
+threads = None
+# If you want to use a single threaded test
+# threads = 1
+s = speedtest.Speedtest()
+s.get_servers(servers)
+s.get_best_server()
+s.download(threads=threads)
+s.upload(threads=threads)
+s.results.share()
+
+results_dict = s.results.dict()
+print(results_dict)
